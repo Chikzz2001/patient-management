5b845facefd23e0369a8ad6f01b0967c585df0e6
Chikzz2001 <dibyabrata1234@gmail.com>
2025-04-18 14:50:34 +0530

    Added auth-service :zap:

---
+++ auth-service/src/main/java/com/pm/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,10 @@
+package com.pm.authservice.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class LoginResponseDTO {
+    private final String token;
+}
