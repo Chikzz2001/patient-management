d141dd35dfe2d2f21aa7e097b6feeb45ff54beca
Chikzz2001 <dibyabrata1234@gmail.com>
2025-04-23 01:19:34 +0530

    [feat]: added service-discovery using netflix-eureka for multi-instance service registration :zap:

--- patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -3,6 +3,10 @@ package com.pm.patientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+By having spring-cloud-starter-netflix-eureka-client on the classpath, your application automatically registers with the Eureka Server. Configuration is required to locate the Eureka server
+ */
+
 @SpringBootApplication
 public class PatientServiceApplication {
 
